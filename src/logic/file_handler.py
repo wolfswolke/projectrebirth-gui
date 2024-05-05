da31149112eb7d1d31c8d7caa39bb6b897c402ed
ZKWolf <zkwolf@zkwolf.com>
2024-05-05 14:04:45 +0200

    Added some basic File handeling logic

--- src/logic/file_handler.py
+++ src/logic/file_handler.py
@@ -1,3 +1,60 @@
+import os
+from logic.api_handler import api_handler
+
+
+class File_Handler:
+    def __init__(self):
+        self.path = ""
+
+    def setup(self, config):
+        self.path = config["path"]
+
+    def delete(self, files):
+        for file in files:
+            os.remove(self.path + file)
+
+    def move(self, files):
+        for file in files:
+            os.rename(self.path + file["source"] + file["name"], self.path + file["location"] + file["name"])
+
+    def download(self, files_ids):
+        for file_id in files_ids:
+            file = api_handler.download_file(file_id)
+            with open(self.path + file_id, "wb") as f:
+                f.write(file.content)
+
+    def download_base_game(self, provider, content):
+        if provider == "steam":
+            if content["mode"] == "live":
+                steamid = content["app_id"]
+                os.system(f"steam://run/{steamid}")
+                return True
+            else:
+                # todo add either STEAM logic or DepotDownloader logic here
+                app_id = content["app_id"]
+                depot_id = content["depot_id"]
+                manifest_id = content["manifest_id"]
+                pass
+        elif provider == "battlenet":
+            # todo add code to download bnetinstaller
+            product = content["product"]
+            uid = content["uid"]
+            lang = content["lang"]
+            pass
+        elif provider == "epic":
+            pass
+        elif provider == "origin":
+            pass
+        elif provider == "uplay":
+            pass
+        elif provider == "web":
+            pass
+        else:
+            return None
+
+
+file_handler = File_Handler()
+
 # Patching instructions would look like this:
 # steam = {"app_id": 480,
 # "depot_id": 481,
