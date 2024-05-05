74675c73f7fa96e89e7ea61faf06fceef53e5dc3
ZKWolf <zkwolf@zkwolf.com>
2024-05-05 16:48:06 +0200

    Playing around with Setting window and add website button

--- src/logic/window_handler.py
+++ src/logic/window_handler.py
@@ -1,4 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, QWidget, QHBoxLayout, QLabel
+import os
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, \
+    QWidget, QHBoxLayout, QLabel, QDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QPixmap
 import sys
 
@@ -25,13 +28,13 @@ class MainWindow(QMainWindow):
         button1.clicked.connect(self.show_subview)
         sidebar_layout.addWidget(button1)
 
-        button2 = QPushButton("Open Settings")
-        button2.clicked.connect(self.show_subview)
-        sidebar_layout.addWidget(button2)
+        button_settings = QPushButton("Open Settings")
+        button_settings.clicked.connect(self.open_settings_window)
+        sidebar_layout.addWidget(button_settings)
 
-        button3 = QPushButton("Open Website")
-        button3.clicked.connect(self.show_subview)
-        sidebar_layout.addWidget(button3)
+        button_website = QPushButton("Open Website")
+        button_website.clicked.connect(self.open_website)
+        sidebar_layout.addWidget(button_website)
 
         main_section_widget = QWidget()
         main_section_layout = QVBoxLayout(main_section_widget)
@@ -66,5 +69,37 @@ class MainWindow(QMainWindow):
             description_item = QTableWidgetItem(item["description"])
             self.table.setItem(i, 2, description_item)
 
+
     def show_subview(self):
         pass
+
+    def open_website(self):
+        os.system("start https://projectrebirth.net")
+        pass
+
+    def open_settings_window(self):
+        settings_window = SettingsWindow()
+        settings_window.exec_()
+
+class SettingsWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Settings")
+        self.setGeometry(300, 300, 300, 200)
+
+        layout = QVBoxLayout()
+
+        button1 = QPushButton("Button 1")
+        layout.addWidget(button1)
+
+        button2 = QPushButton("Button 2")
+        layout.addWidget(button2)
+
+        path_label = QLabel("Text input")
+        layout.addWidget(path_label)
+
+        self.path_input = QLineEdit()
+        layout.addWidget(self.path_input)
+
+        self.setLayout(layout)
