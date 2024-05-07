164c3b6018aaa3c670ac5b197750c6af2f7b6ff5
zkwolf <zkwolf@zkwolf.stream>
2024-05-07 16:05:06 +0200

    added config updater

--- src/logic/setup_handlers.py
+++ src/logic/setup_handlers.py
@@ -5,3 +5,13 @@ def load_config():
     with open('config//gui_config.yaml', 'r') as f:
         config_file = yaml.safe_load(f)
     return config_file
+
+
+def update_config_item(key, value, sub_key=None):
+    with open('config//gui_config.yaml', 'r') as f:
+        config_file = yaml.safe_load(f)
+    if sub_key is None:
+        config_file[key] = value
+    else:
+        config_file[key][sub_key] = value
+
